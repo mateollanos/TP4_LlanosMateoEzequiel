61b23ab48bc4942b23a2ed5faefbd064a0d3c6db
mateoo <mateoo@DESKTOP-U5ED9AM>
2024-05-29 03:14:49 -0300

    avance del TP 4

---
+++ src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.model;
+
+import java.time.LocalDate;
+
+public class Alumno {
+	private String dni;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String telefono;
+	private LocalDate fecha_nac;
+	private String domicilio;
+	private String lu;
+}
---
+++ src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.model;
+
+import ar.edu.unju.fi.model.Carrera;
+import ar.edu.unju.fi.model.Docente;
+
+public class Materia {
+	private String codigo;
+	private String nombre;
+	private String Curso;
+	private int horas;
+	private String modalidad;
+	private Docente docente;
+	private Carrera carrera;
+
+	
+
+}
